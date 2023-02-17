3944a54a812a0acce886b0e4f188520ef277f989
Yiaourt <loic.bois.07@gmail.com>
2023-02-17 07:37:01 +0100

    Launcher Minecraft TekCity

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module fr.tekcity.tekcity_launcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires eu.hansolo.fx.countries;
+    requires eu.hansolo.fx.heatmap;
+    requires eu.hansolo.toolboxfx;
+    requires eu.hansolo.toolbox;
+
+    opens fr.tekcity.tekcity_launcher to javafx.fxml;
+    exports fr.tekcity.tekcity_launcher;
+}
